ad93ac1ffa76227d5e5316a15cd7e6f559a66f9a
pc <stikvrn@gmail.com>
2020-09-17 17:01:50 +0300

    1. Модульные тесты. [#360118]

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -3,11 +3,11 @@ package ru.job4j.converter;
 public class Converter {
 
         public static int rubleToEuro(int value) {
-          return -2;
+          return value / 70;
         }
 
     public static int rubleToDollar(int value) {
-            return -1;
+            return value / 60;
         }
 
         public static void main(String[] args) {
