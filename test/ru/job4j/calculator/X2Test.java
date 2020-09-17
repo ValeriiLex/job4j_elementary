2cb0629636cc05e10035d4e870391f78aaba3419
pc <stikvrn@gmail.com>
2020-09-17 14:06:36 +0300

    1.1. Входные данные в тесте [#360123]

---
+++ test/ru/job4j/calculator/X2Test.java
@@ -0,0 +1,70 @@
+package ru.job4j.calculator;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class X2Test {
+
+    @Test
+    public void whenA10B0C0X2Then40() {
+
+        int a = 10;
+        int b = 0;
+        int c = 0;
+        int x = 2;
+        int expected = 40;
+        int rsl = X2.calc(a, b, c, x);
+        System.out.println(rsl);
+        Assert.assertEquals(expected, rsl);
+    }
+
+    @Test
+    public void whenA0BCX2() {
+        int a = 0;
+        int b = 1;
+        int c = 1;
+        int x = 1;
+        int expected = 40;
+        int rsl = X2.calc(a, b, c, x);
+        System.out.println(rsl);
+        Assert.assertEquals(expected, rsl);
+    }
+
+    @Test
+    public void whenABCX1() {
+        int a = 1;
+        int b = 1;
+        int c = 1;
+        int x = 1;
+        int expected = 40;
+        int rsl = X2.calc(a, b, c, x);
+        System.out.println(rsl);
+        Assert.assertEquals(expected, rsl);
+    }
+
+    @Test
+    public void whenABX1C0() {
+        int a = 1;
+        int b = 1;
+        int c = 0;
+        int x = 1;
+        int expected = 40;
+        int rsl = X2.calc(a, b, c, x);
+        System.out.println(rsl);
+        Assert.assertEquals(expected, rsl);
+    }
+
+    @Test
+    public void whenABC1X0() {
+        int a = 1;
+        int b = 1;
+        int c = 1;
+        int x = 0;
+        int expected = 40;
+        int rsl = X2.calc(a, b, c, x);
+        System.out.println(rsl);
+        Assert.assertEquals(expected, rsl);
+
+    }
+
+}
