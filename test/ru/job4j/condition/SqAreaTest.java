e15ceb9a84c3047f586bd8ceb0f350bf1cf8b65c
pc <stikvrn@gmail.com>
2020-09-22 16:52:20 +0300

    4.5. Отладка программы в IDEA [#360034]

--- test/ru/job4j/condition/SqAreaTest.java
+++ test/ru/job4j/condition/SqAreaTest.java
@@ -7,9 +7,9 @@ public class SqAreaTest {
 
     @Test
     public void square() {
-        int p = 6;
-        int k = 7;
-        double exp = 0.984375;
+        int p = 4;
+        int k = 1;
+        double exp = 1;
         double res = SqArea.square(p, k);
         Assert.assertEquals(res, exp, 0.01);
     }
