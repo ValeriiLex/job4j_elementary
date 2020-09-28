380c3cf73eca4bff51ed757d2b4c93c260c5bcee
pc <stikvrn@gmail.com>
2020-09-28 13:04:40 +0300

    5.4. Протеиновая диета [#360142]

---
+++ test/ru/job4j/loop/FitnessTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class FitnessTest {
+    @Test
+    public void whenIvanGreatNik() {
+        int month = Fitness.calc(90, 90);
+        assertThat(month, is(1));
+    }
+
+    @Test
+    public void whenIvanLessByOneNik() {
+        int month = Fitness.calc(90, 95);
+        assertThat(month, is(1));
+        System.out.println(month);
+    }
+
+    @Test
+    public void whenIvanLessByFewNik() {
+        int month = Fitness.calc(10, 50);
+        assertThat(month, is(4));
+    }
+}
