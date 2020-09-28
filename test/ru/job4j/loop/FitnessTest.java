ed2ed4c58ae4813ff6938a499cf3262d0164fc23
pc <stikvrn@gmail.com>
2020-09-28 19:31:18 +0300

    5.5. Простое число [#360145]

--- test/ru/job4j/loop/FitnessTest.java
+++ test/ru/job4j/loop/FitnessTest.java
@@ -3,12 +3,15 @@ package ru.job4j.loop;
 import org.junit.Test;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
 
 public class FitnessTest {
     @Test
     public void whenIvanGreatNik() {
         int month = Fitness.calc(90, 90);
+
         assertThat(month, is(1));
+        System.out.println(month);
     }
 
     @Test
