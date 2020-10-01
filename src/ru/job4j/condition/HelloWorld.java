fba3527ff4f28065aa010117ff11d5943f163ae7
pc <stikvrn@gmail.com>
2020-10-01 21:18:12 +0300

    Hello World

---
+++ src/ru/job4j/condition/HelloWorld.java
@@ -0,0 +1,17 @@
+package ru.job4j.condition;
+
+public class HelloWorld {
+    public static String checkNumber(int number) {
+
+        if (number % 3 == 0 && number % 5 == 0) {
+            return ("Hello, World!!!");
+        } else if (number % 5 == 0) {
+            return ("World");
+        } else if (number % 3 == 0) {
+            return "Hello";
+        } else {
+            return "Operation not support";
+        }
+
+    }
+}
