853cb65ee4a9e70aa12ea87befa8519ba4ade510
pc <stikvrn@gmail.com>
2020-10-08 12:05:34 +0300

    Copy - Hello

---
+++ src/ru/job4j/free/Copy.java
@@ -0,0 +1,10 @@
+package ru.job4j.free;
+
+public class Copy {
+    public static void main(String[] args) {
+        String words = "hello";
+        for (int i = 0; i < 3; i++) {
+            System.out.println(words);
+        }
+    }
+}
