8168318cf73ad611c2d938a9be35443b81ad25ef
pc <stikvrn@gmail.com>
2020-10-09 13:19:54 +0300

    Projekt - day 1

---
+++ src/projekt/SaveStudentOrder.java
@@ -0,0 +1,17 @@
+package projekt;
+
+public class SaveStudentOrder {
+
+    public static void main(String[] args) {
+        saveStudentOrder();
+        scheduleStudentOrder();
+    }
+
+    static void saveStudentOrder() {
+        System.out.println("saveStudentOrder 1");
+    }
+
+    static void scheduleStudentOrder() {
+        System.out.println("Расписание");
+    }
+}
