c9908294decb8dae3d8f0240127ab09fbdb6ac14
pc <stikvrn@gmail.com>
2020-10-16 13:27:14 +0300

    12244

--- src/projekt/StudentOrder.java
+++ src/projekt/StudentOrder.java
@@ -1,8 +1,40 @@
 package projekt;
 
 public class StudentOrder {
-    String hFirstName;
-    String hLastName;
-    String wFirstName;
-    String wLastName;
+   private String hFirstName;
+    private String hLastName;
+    private String wFirstName;
+    private String wLastName;
+
+    public String gethFirstName() {
+        return hFirstName;
+    }
+
+    public void sethFirstName(String hFirstName) {
+        this.hFirstName = hFirstName;
+    }
+
+    public String gethLastName() {
+        return hLastName;
+    }
+
+    public void sethLastName(String hLastName) {
+        this.hLastName = hLastName;
+    }
+
+    public String getwFirstName() {
+        return wFirstName;
+    }
+
+    public void setwFirstName(String wFirstName) {
+        this.wFirstName = wFirstName;
+    }
+
+    public String getwLastName() {
+        return wLastName;
+    }
+
+    public void setwLastName(String wLastName) {
+        this.wLastName = wLastName;
+    }
 }
---
+++ src/ru/job4j/array/Merge.java
@@ -0,0 +1,20 @@
+package ru.job4j.array;
+
+public class Merge {
+    public static int[] merge(int[] left, int[] right) {
+        int[] rsl = new int[left.length + right.length];
+       int i = 0;
+       int j = 0;
+       int x = 0;
+    while (i < left.length && j < right.length) {
+        rsl[x++] = left[i] < right[j] ? left[i++] : right[j++];
+    }
+    while (i < left.length) {
+        rsl[x++] = left[i++];
+    }
+    while (j < right.length) {
+        rsl[x++] = right[j++];
+    }
+        return rsl;
+    }
+}
