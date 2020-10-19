e950a6ba689d596464cba7518e8c3d85b7373cac
pc <stikvrn@gmail.com>
2020-10-19 10:55:29 +0300

    1.5. Поля объекта [#360246]

--- src/ru/job4j/oop/Cat.java
+++ src/ru/job4j/oop/Cat.java
@@ -2,12 +2,33 @@ package ru.job4j.oop;
 
 public class Cat {
 
-    public void music() {
-        System.out.println("Tra tra tra");
+    private String food;
+    private String name;
+
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
+
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
+    public void show() {
+        System.out.println(this.food + this.name);
     }
 
     public static void main(String[] args) {
-        Cat petya = new Cat();
-        petya.music();
+        System.out.println("There are gav's food.");
+        Cat gav = new Cat();
+        gav.eat("kotleta");
+        gav.giveNick(" Chen");
+        gav.show();
+
+        System.out.println("There are black's food.");
+        Cat black = new Cat();
+        black.eat("fish");
+        black.giveNick(" Back");
+        black.show();
+
     }
 }
